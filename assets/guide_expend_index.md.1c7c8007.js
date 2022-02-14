067c65c0dde1c4b157d2eeb8d2fec6b08c3abc69
Gavin <gavin.zhou@w-group.com.cn>
2022-02-14 11:44:22 +0800

    feat@cain

---
+++ assets/guide_expend_index.md.1c7c8007.js
@@ -0,0 +1,48 @@
+import{_ as e,c as n,o as t,a as i}from"./app.2d715e14.js";const f='{"title":"\u9632\u91CD\u63D0\u4EA4","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u9632\u91CD\u63D0\u4EA4","slug":"\u9632\u91CD\u63D0\u4EA4"},{"level":3,"title":"\u5173\u4E8E\u9632\u91CD\u63D0\u4EA4","slug":"\u5173\u4E8E\u9632\u91CD\u63D0\u4EA4"},{"level":3,"title":"tips","slug":"tips"},{"level":3,"title":"\u6D41\u7A0B\u5173\u7CFB","slug":"\u6D41\u7A0B\u5173\u7CFB"},{"level":3,"title":"\u914D\u7F6E\u5408\u5E76\u63D0\u9192","slug":"\u914D\u7F6E\u5408\u5E76\u63D0\u9192"},{"level":3,"title":"\u793A\u4F8B\u4EE3\u7801","slug":"\u793A\u4F8B\u4EE3\u7801"}],"relativePath":"guide/expend/index.md","lastUpdated":1644809925039}',s={},a=i(`<h2 id="\u9632\u91CD\u63D0\u4EA4" tabindex="-1">\u9632\u91CD\u63D0\u4EA4 <a class="header-anchor" href="#\u9632\u91CD\u63D0\u4EA4" aria-hidden="true">#</a></h2><h3 id="\u5173\u4E8E\u9632\u91CD\u63D0\u4EA4" tabindex="-1">\u5173\u4E8E\u9632\u91CD\u63D0\u4EA4 <a class="header-anchor" href="#\u5173\u4E8E\u9632\u91CD\u63D0\u4EA4" aria-hidden="true">#</a></h3><p>\u5728\u6211\u4EEC\u65E5\u5E38\u751F\u6D3B\u4E2D\u4F1A\u51FA\u73B0\u67D0\u4E9B\u9700\u6C42\u540E\u53F0\u9700\u8981\u4F60\u505A\u9632\u91CD\u7684\u8868\u5355\u63D0\u4EA4,\u8FD9\u91CC\u6211\u4EEC\u63D0\u4F9B\u4E86\u89E3\u51B3\u65B9\u6848,\u5E76\u4E14\u6E90\u7801\u4E2D\u4E5F\u4E3A\u6B64\u9884\u7559\u4E86\u4E00\u4E9B\u914D\u7F6E\u6765\u6EE1\u8DB3\u4E00\u4E9B\u5B9A\u5236\u5316\u9700\u6C42\u66F4\u53D8\u3002</p><h3 id="tips" tabindex="-1">tips <a class="header-anchor" href="#tips" aria-hidden="true">#</a></h3><div class="tip custom-block"><p class="custom-block-title">TIP</p><p>\u7531\u4E8E\u662F\u57FA\u4E8E\u62E6\u622A\u5668\u7684\u89E3\u51B3\u65B9\u6848,\u4E3A\u4E86\u66F4\u7075\u6D3B\u7684\u7EC4\u7EC7\u9632\u91CD\u4EE3\u7801,\u6240\u4EE5\u90E8\u5206\u9632\u91CD\u529F\u80FD\u5E76\u6CA1\u6709\u5408\u5E76\u5230vxiosAPI\u5185,\u8FD9\u6837\u5927\u5BB6\u5C31\u53EF\u4EE5\u6839\u636E\u81EA\u5DF1\u7684\u9700\u6C42\u53BB\u7EC4\u7EC7\u81EA\u5DF1\u9700\u8981\u7684\u9632\u91CD\u4EE3\u7801</p></div><h3 id="\u6D41\u7A0B\u5173\u7CFB" tabindex="-1">\u6D41\u7A0B\u5173\u7CFB <a class="header-anchor" href="#\u6D41\u7A0B\u5173\u7CFB" aria-hidden="true">#</a></h3><div class="info custom-block"><p class="custom-block-title">INFO</p><p>\u793A\u4F8B\u521B\u5EFA --&gt; \u53D1\u51FA\u8BF7\u6C42--&gt; \u6DFB\u52A0\u5F53\u524D\u8BF7\u6C42\u6807\u8BC6\u5230pendding\u4E2D --&gt; \u8BF7\u6C42\u7ED3\u675F --&gt; \u4ECEpendding\u4E2D\u6E05\u9664\u5F53\u524D\u8BF7\u6C42\u6807\u8BC6</p></div><h3 id="\u914D\u7F6E\u5408\u5E76\u63D0\u9192" tabindex="-1">\u914D\u7F6E\u5408\u5E76\u63D0\u9192 <a class="header-anchor" href="#\u914D\u7F6E\u5408\u5E76\u63D0\u9192" aria-hidden="true">#</a></h3><div class="warning custom-block"><p class="custom-block-title">WARNING</p><p>\u{1F3A9}\u7531\u4E8E<a href="/guide/advanced/#\u5C31\u8FD1\u539F\u5219"><code>\u5C31\u8FD1\u539F\u5219</code></a>\u62E6\u622A\u5668\u7684\u914D\u7F6E\u4F1A\u4F18\u5148\u4E0E\u53D1\u8D77\u8BF7\u6C42\u65F6task\u7684\u914D\u7F6E,\u5982\u679C\u5C06\u9632\u91CD\u5B9A\u4E49\u5728\u5168\u5C40,\u8BF7\u91CD\u65B0create\u4E00\u4E2Avxios\u5B9E\u4F8B\u8FDB\u884C\u4E2A\u522B\u8BF7\u6C42\u5904\u7406\u3002</p></div><h3 id="\u793A\u4F8B\u4EE3\u7801" tabindex="-1">\u793A\u4F8B\u4EE3\u7801 <a class="header-anchor" href="#\u793A\u4F8B\u4EE3\u7801" aria-hidden="true">#</a></h3><div class="language-"><pre><code>//\u5F15\u5165vxios\u6838\u5FC3
+const vxios = require(&#39;@gatomis/vxios&#39;)
+//\u5EFA\u7ACB\u4E00\u4E2Apending Map
+const pending=new Map();
+//\u8FD4\u56DE\u53C2\u6570\u65B9\u6CD5
+const getUrl=config=&gt;[config.method,config.url].join(&#39;&amp;&#39;);
+//\u521B\u5EFA\u4E00\u4E2A\u6DFB\u52A0pending\u7684\u65B9\u6CD5
+const addPending=(config)=&gt;{
+  const url=getUrl(config)
+  if(!pending)return
+  if(pending.has(url)){
+    config.getTask=task=&gt;task.abort()
+  }else{
+    pending.clear()
+    pending.set(url)
+  }
+}
+//\u79FB\u9664\u4E00\u4E2A\u5220\u9664pending\u7684\u65B9\u6CD5
+const removePending=config=&gt;{
+  const url=getUrl(config)
+  if(!pending)return
+  if(pending.has(url)){
+      pending.delete(url)
+  }
+  console.log(pending,url);
+}
+
+//\u521B\u5EFA\u4E00\u4E2A\u81EA\u5DF1\u7684\u5B9E\u4F8B
+const myVxios = vxios.create({
+  baseURL: &#39;https://www.escook.cn&#39;
+})
+//\u6DFB\u52A0\u8BF7\u6C42\u62E6\u622A\u5668  req
+myVxios.interceptors.request.use(config =&gt; {
+  console.log(config.url);
+   addPending(config)
+  return config
+})
+\u6DFB\u52A0\u54CD\u5E94\u62E6\u622A\u5668 resp
+myVxios.interceptors.response.use(res =&gt; {
+  console.log(res);
+   removePending(res.config)
+  return res
+},err=&gt;{
+  //\u51FA\u73B0\u6216\u62A5\u9519\u540EN\u79D2\u540E\u79FB\u9664\u9632\u91CD
+  console.error(err);
+  setTimeout(()=&gt; removePending(err.config),5000)
+})
+</code></pre></div>`,11),r=[a];function o(d,c,l,g,p,h){return t(),n("div",null,r)}var v=e(s,[["render",o]]);export{f as __pageData,v as default};
